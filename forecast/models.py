e518af8613dda1c2f58b6b74864e021dc8be09e1
Sabinodm <sabinodemaio@gmail.com>
2025-06-16 18:43:25 +0200

    FEAT: Added models

--- forecast/models.py
+++ forecast/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class ForecastQuery(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    result = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.location} ({self.date} {self.time})"
