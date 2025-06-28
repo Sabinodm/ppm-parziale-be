2337660268d1d2e4c1f2be0418b2e37b6d8d8cf7
Sabinodm <sabinodemaio@gmail.com>
2025-06-28 12:51:22 +0200

    FEAT: added function for creating new forecasts from admin

--- forecast/admin.py
+++ forecast/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import ForecastData
+
+@admin.register(ForecastData)
+class ForecastDataAdmin(admin.ModelAdmin):
+    list_display = ['location', 'date', 'time', 'result']
+    search_fields = ['location']
