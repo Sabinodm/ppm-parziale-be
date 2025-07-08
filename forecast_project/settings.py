b5093192feccac1b861f4318ae04908f7f0f49f8
Sabinodm <sabinodemaio@gmail.com>
2025-07-08 10:58:50 +0200

    FIX: fixed allowed host

--- forecast_project/settings.py
+++ forecast_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1n+*&&_%9n5arte^!y(2_-%u_v1ys$ty5pa)#rhtgc2(3#1m$y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
